2c76db353a34e910f0fc0da4708c021eb9c0050d
Cameron Lopez <cslopez1@berkeley.edu>
2020-04-17 14:02:45 -0700

    build test

--- test/edu/berkeley/aep/FinalTest.java
+++ test/edu/berkeley/aep/FinalTest.java
@@ -1,6 +1,12 @@
 package edu.berkeley.aep;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class FinalTest {
+    @Test
+    public void initialTest() {
+        Final finalTest = new Final();
+        //assertEquals("test", finalTest);
+    }
 }
